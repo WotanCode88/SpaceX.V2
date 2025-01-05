7035bd30c96ff498784872806a7091277490b97a
The Last Gauleiter <killkenny.xd@gmail.com>
2025-01-05 23:35:07 +0300

    Initial Commit

---
+++ SpaceX.V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX.V2
+//
+//  Created by The Last Gauleiter on 05.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
